a5b79add8eaccf9fb6201d07136cd7ecdf5cd178
stevenisaacgo <stevenisaacgo03@gmail.com>
2024-06-17 23:04:02 +0200

    chore: Update dependencies and add Skills component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import user from '../assets/user.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import user from "../assets/user.svg";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow">
+    <nav className="bg-gray-900 text-white shadow">
       <div className="container mx-auto px-6 py-3">
         <div className="flex justify-between items-center">
           <div className="flex items-center space-x-2 text-lg font-bold">
             <img src={user} alt="usericon" className="w-10 h-10 rounded-full" />
-            <Link to="/">Isaac Godoy Ortega</Link>
+            <Link to="/">DevelopedbyIsaac</Link>
           </div>
           <div className="hidden md:flex space-x-4">
-            <Link to="/projects" className="text-gray-600 hover:text-blue-600">Projects</Link>
-            <Link to="/about" className="text-gray-600 hover:text-blue-600">About</Link>
-            <Link to="/contact" className="text-gray-600 hover:text-blue-600">Contact</Link>
+            <Link to="/projects" className=" hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500">
+              Projects
+            </Link>
+            <Link to="/about" className=" hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500">
+              About
+            </Link>
+            <Link to="/contact" className=" hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500">
+              Contact
+            </Link>
           </div>
         </div>
       </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../Hero.js';
+import Skills from '../Skills.js';
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Skills />
     </div>
   );
 };
